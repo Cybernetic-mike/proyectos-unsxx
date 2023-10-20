6b91dcb082ef9b352adaed73f6436d1780234ec1
Miguel Callizaya <miguel.callizaya@gmail.com>
2023-10-20 11:16:39 -0400

    frontend url

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
 export const TOKEN_SECRET = "clave secreta"
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000"
--- src/db.js
+++ src/db.js
@@ -1,10 +1,5 @@
 import mongoose from "mongoose";
 
-
-//run().catch(console.dir);*/
-
-//import mongoose from 'mongoose'
-
 export const connectDB = async()=>{
   try{
       await mongoose.connect('mongodb+srv://mcallizaya:Cyb3rn3t!c@cluster0.qofexn3.mongodb.net/proyectos_unsxx?retryWrites=true&w=majority');
